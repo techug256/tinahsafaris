359a3f394f484a4ea3ab9a7c0fa943589e59ef6f
techug256 <dicksoncruz70@gmail.com>
2026-09-21 12:44:43 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(async ({ mode }) => {
   }
 
   return {
+    base:'/tinahsafaris/'
     plugins,
     envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
     define: processEnvDefines,
